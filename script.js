ecc0dce9eeb647f2b8064b18ea1cfdd35240b9a4
Marialaina <pmarialaina@gmail.com>
2020-12-22 09:15:29 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+//https://api.krzana.com/v3/publications?channel_ids[]=32735&limit=40
+
+//make a ajax request
+
+// $.ajax({
+//     url: "https://api.krzana.com/v3/publications?channel_ids[]=32735&limit=40",
+//     // Tell YQL what we want and that we want JSON
+//     data: {
+//         // q: "select title,abstract,url from search.news where query=\"cat\"",
+//         format: "json"
+//     },
+//     // Work with the response
+//     success: function( response ) {
+//         console.log( response ); // server response
+//         //loop over the response array
+//         //create h3 elements
+//         //insert the response sub i (from the for loop). text
+//     }
+// })
+
+$.ajax({url:"https://api.krzana.com/v3/publications?channel_ids[]=32735&limit=40"}).then( data => {
+    console.log(data);
+    const article = data.map(article => {
+        return {
+            author: article.author_name,
+            url: article.source_url,
+
+        }
+    })
+})
+
+
+
