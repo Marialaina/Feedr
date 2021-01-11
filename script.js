09152a5a88e4b80a8cfffdf35b8f62ee41380a89
Marialaina <pmarialaina@gmail.com>
2021-01-11 10:23:30 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,26 +5,40 @@ $.ajax({url:"https://api.krzana.com/v3/publications?channel_ids[]=32735&limit=40
       // $('.featuredImage').html(article.media);
       // $(`#article-${idx} a`).attr('href', article.source_url);
       // $(`#article-${idx} .impressions`).html(article.origin);
+      
       const art = document.createElement('article');
       const section = document.createElement('section');
       const ah = document.createElement('a');
       const title = document.createElement('h3');
-      const auth = document.createElement('p');
+      const teaser = document.createElement('p');
       const fix = document.createElement('div');
-      
-      
+      const url = document.createElement('h4');
+     
+
+      url.textContent = article.author_url;
       art.classList.add('article');
-      section.classList.add('articleContent');
-      ah.setAttribute('href', article.source_url);
-      title.textContent = article.origin;
-      ah.append(title);
-      section.append(ah);
-      auth.textContent = article.author_name;
-      auth.classList.add('author');
-      section.append(auth);
-      fix.classList.add('clearfix');
       art.append(section);
       art.append(fix);
       document.querySelector('#main').append(art);
+      
+      console.log();
+      section.classList.add('articleContent');
+      
+      ah.setAttribute('href', article.source_url);
+      ah.append(title);
+      section.append(ah);
+      
+      title.textContent = article.origin;
+      
+      
+      teaser.textContent = article.teaser_text;
+      teaser.classList.add('teaser');
+      section.append(teaser);
+      
+      console.log();
+      
+      fix.classList.add('clearfix');
+      
+      
     })
 })
