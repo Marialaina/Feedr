ae170c90504d2279157a799fac46a73fc7affb5a
Marialaina <pmarialaina@gmail.com>
2021-01-12 20:14:55 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $.ajax({url:"https://api.krzana.com/v3/publications?channel_ids[]=32735&limit=40"}).then( data => {
     const article = data.forEach((article, idx) => {
-      console.log(article);
+      // console.log(article);
       // $(`#article-${idx} .author`).html(article.author_name);
       // $('.featuredImage').html(article.media);
       // $(`#article-${idx} a`).attr('href', article.source_url);
@@ -20,12 +20,19 @@ $.ajax({url:"https://api.krzana.com/v3/publications?channel_ids[]=32735&limit=40
       const section = document.createElement('section');
       const ah = document.createElement('a');
       const title = document.createElement('h3');
+      
       const teaser = document.createElement('p');
+      //google limit p tags text html
+      // if (article.teaser_text) {
+      //   teaser.text = teaser_tex
+      // }
+
       const fix = document.createElement('div');
       const url = document.createElement('h4');
      
 
       url.textContent = article.author_url;
+      
       art.classList.add('article');
       
       art.append(section2);
