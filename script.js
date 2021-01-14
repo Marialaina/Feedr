4b8b38255b5e67e0f57572c9f9416ad9d22e335f
Marialaina <pmarialaina@gmail.com>
2021-01-13 21:00:41 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -14,15 +14,23 @@ $.ajax({url:"https://api.krzana.com/v3/publications?channel_ids[]=32735&limit=40
       const section2 = document.createElement('section');
       section2.classList.add('featuredImage');
       section2.append(img);
-
-
-      const art = document.createElement('article');
       const section = document.createElement('section');
+      const link  = document.createElement('h6');
+    
+      link.textContent = article.source_url;
+      link.classList.add('h6');
+      
+
+      
+      const art = document.createElement('article');
+      
       const ah = document.createElement('a');
       const title = document.createElement('h3');
       
       const teaser = document.createElement('p');
       //google limit p tags text html
+      //split the string into the array
+      //.splice 
       // if (article.teaser_text) {
       //   teaser.text = teaser_tex
       // }
@@ -34,9 +42,9 @@ $.ajax({url:"https://api.krzana.com/v3/publications?channel_ids[]=32735&limit=40
       url.textContent = article.author_url;
       
       art.classList.add('article');
-      
       art.append(section2);
       art.append(section);
+      
       art.append(fix);
       
       document.querySelector('#main').append(art);
@@ -47,11 +55,16 @@ $.ajax({url:"https://api.krzana.com/v3/publications?channel_ids[]=32735&limit=40
       ah.setAttribute('href', article.source_url);
       ah.append(title);
       section.append(ah);
+      section.append(link);
+
+     
       
       title.textContent = article.origin;
       
+      //if the character i end on is not a space i want to iterate over the remaining string until i get to a space.
+      //find character 150 which is going to be [] 150 and from there have a while loop or do loop until i find the space.
       
-      teaser.textContent = article.teaser_text;
+      teaser.textContent = article.teaser_text.slice(0, 150) + "...";
       teaser.classList.add('teaser');
       section.append(teaser);
       
